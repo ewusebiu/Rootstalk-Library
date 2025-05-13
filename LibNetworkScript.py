64f7358941ad7e9ddb4c62a0593668ae51f9c6f3
BertrandFlanet <bertrand.flanet@gmail.com>
2025-05-13 22:05:10 +0200

    Set host and port for Render deployment

--- LibNetworkScript.py
+++ LibNetworkScript.py
@@ -553,5 +553,10 @@ def update_graph(selected_node, selected_theme, hovered_node):
 
     return fig
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
